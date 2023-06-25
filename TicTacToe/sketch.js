04c0d48f4d87b24086ef185f4ba40e71ea5ee4ad
Mehdi <mehdi0341@gmail.com>
2023-06-25 17:32:52 +0200

    x_o_tie

--- TicTacToe/sketch.js
+++ TicTacToe/sketch.js
@@ -51,6 +51,49 @@ function draw() {
       }
     }
   }
+
+  let result = checkWinner();
+  if (result != false) {
+    console.log(result);
+    noLoop();
+  } else {
+    NextTurn();
+  }
+}
+
+function equal3(a, b, c) {
+  return (a == b && a == c && a != '');
+}
+
+function checkWinner() {
+  let winner = false;
+  for (let i = 0; i < 3; i++) {
+    if (equal3(board[i][0], board[i][1], board[i][2])) {
+      winner = board[i][0];
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (equal3(board[0][i], board[1][i], board[2][i])) {
+      winner = board[0][i];
+    }
+  }
+
+  if (equal3(board[0][0], board[1][1], board[2][2])) {
+    winner = board[0][0];
+  }
+
+  if (equal3(board[0][2], board[1][1], board[2][0])) {
+    winner = board[0][2];
+  }
+
+
+  if (winner == false && available.length == 0) {
+    return 'tie';
+  }
+  else {
+    return winner;
+  }
 }
 
 function NextTurn() {
@@ -64,6 +107,6 @@ function NextTurn() {
   }
 }
 
-function mousePressed() {
-  NextTurn();
-}
+// function mousePressed() {
+//   NextTurn();
+// }
