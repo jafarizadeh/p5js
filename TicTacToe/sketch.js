acd685510b09abc87780e43f11fa397f4a1ae7c8
Mehdi <mehdi0341@gmail.com>
2023-06-25 10:37:50 +0200

    board

--- TicTacToe/sketch.js
+++ TicTacToe/sketch.js
@@ -1,7 +1,23 @@
+let board = 
+[
+  ['X', 'O', ''],
+  ['', '', ''],
+  ['', '', '']
+]
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(200);
+  background(230);
+  strokeWeight(3);
+  let w = width / 3;
+  let h = height / 3;
+
+  line(w, 0, w, height);
+  line(w * 2, 0, w * 2, height);
+
+  line(0, h, width, h);
+  line(0, h * 2, width, h * 2);
 }
